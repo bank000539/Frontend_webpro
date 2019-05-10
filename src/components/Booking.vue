35153cbcb3a215cea9559091843e27009e9b92a8
rinriko <rinrikosung@hotmail.com>
2019-05-11 00:52:19 +0700

    remove connsole

--- src/components/Booking.vue
+++ src/components/Booking.vue
@@ -624,7 +624,7 @@ export default {
       doc.setFontType("normal");
       var verticalOffset = 150;
       for (var i = 0; i < this.Book.equipments.length; i++) {
-        console.log(this.Book.equipments[i]);
+        // console.log(this.Book.equipments[i]);
         doc.text(40, verticalOffset, this.Book.equipments[i].name);
         doc.setFontSize(20);
         doc.setFontType("bold");
@@ -642,7 +642,7 @@ export default {
       //   "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus id eros turpis. Vivamus tempor urna vitae sapien mollis molestie. Vestibulum in lectus non enim bibendum laoreet at at libero. Etiam malesuada erat sed sem blandit in varius orci porttitor. Sed at sapien urna. Fusce augue ipsum, molestie et adipiscing at, varius quis enim. Morbi sed magna est, vel vestibulum urna. Sed tempor ipsum vel mi pretium at elementum urna tempor. Nulla faucibus consectetur felis, elementum venenatis mi mollis gravida. Aliquam mi ante, accumsan eu tempus vitae, viverra quis justo.\n\nProin feugiat augue in augue rhoncus eu cursus tellus laoreet. Pellentesque eu sapien at diam porttitor venenatis nec vitae velit. Donec ultrices volutpat lectus eget vehicula. Nam eu erat mi, in pulvinar eros. Mauris viverra porta orci, et vehicula lectus sagittis id. Nullam at magna vitae nunc fringilla posuere. Duis volutpat malesuada ornare. Nulla in eros metus. Vivamus a posuere libero.";
       var loremipsum = this.Book.description;
       var lines = doc.splitTextToSize(loremipsum, 120);
-      console.log(lines);
+      // console.log(lines);
       doc.setFontSize(20);
       doc.setFontType("bold");
       doc.text(20, verticalOffset, "Description:");
